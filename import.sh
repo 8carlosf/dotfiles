c0f6dba7a140e660298440252febfbb87c2da6bb
carlosf <8carlosf@gmail.com>
2014-02-27 21:55:02 +0000

    import.sh minor changes

--- import.sh
+++ import.sh
@@ -6,11 +6,13 @@ cp $HOME/.vimrc                         $homerepo/vimrc
 
 # [Emacs]
 cp $HOME/.emacs                         $homerepo/emacs
-cp $HOME/.emacs.d/ -r                   $homerepo/emacs.d
+cp -r $HOME/.emacs.d/                   $homerepo/emacs.d
 
 # [Bash]
 cp $HOME/.bashrc                        $homerepo/bashrc
 cp $HOME/.Xdefaults                     $homerepo/Xdefaults
+
+# [Xorg]
 cp $HOME/.xinitrc                       $homerepo/xinitrc
 
 # [Subtle]
@@ -20,9 +22,12 @@ cp $HOME/.config/subtle/subtle.rb       $homerepo/subtle.rb
 cp $HOME/.moc/config                    $homerepo/mocconfig
 cp $HOME/.moc/config/themes/theme       $homerepo/moctheme
 
-# [conky]
+# [Conky]
 cp $HOME/.conkyrc                       $homerepo/conkyrc
 
+# [Bashrun2]
 cp $HOME/.config/bashrun2/bashrun2.rc   $homerepo/bashrun2.rc
+
+# [dunstrc]
 cp $HOME/.config/dunstrc                $homerepo/dunstrc
 
