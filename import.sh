4f6a2a1e5376e3e32ac8896b561bada4c10b718c
carlosf <8carlosf@gmail.com>
2014-02-27 21:49:35 +0000

    import.sh edited

--- import.sh
+++ import.sh
@@ -1,15 +1,28 @@
-dir = "$HOME/.dotfiles"
 
-cp $HOME/.vimrc                         ${dir}/dotvimrc
-cp $HOME/.emacs                         ${dir}/dotemacs
-cp $HOME/.emacs.d/ -r                   ${dir}/dotemacs.d
-cp $HOME/.bashrc                        ${dir}/dotbashrc
-cp $HOME/.xinitrc                       ${dir}/dotxinitrc
-cp $HOME/.Xdefaults                     ${dir}/dotXdefaults
-cp $HOME/.config/subtle/subtle.rb       ${dir}/subtle.rb
-cp $HOME/.moc/config                    ${dir}/mocconfig
-cp $HOME/.moc/config/themes/theme       ${dir}/moctheme
-cp $HOME/.conkyrc                       ${dir}/dotconkyrc
-cp $HOME/.config/bashrun2/bashrun2.rc   ${dir}/bashrun2.rc
-cp $HOME/.config/dunstrc                ${dir}/dunstrc
+homerepo = "$HOME/dotfiles"
+
+# [Vim]
+cp $HOME/.vimrc                         $homerepo/vimrc
+
+# [Emacs]
+cp $HOME/.emacs                         $homerepo/emacs
+cp $HOME/.emacs.d/ -r                   $homerepo/emacs.d
+
+# [Bash]
+cp $HOME/.bashrc                        $homerepo/bashrc
+cp $HOME/.Xdefaults                     $homerepo/Xdefaults
+cp $HOME/.xinitrc                       $homerepo/xinitrc
+
+# [Subtle]
+cp $HOME/.config/subtle/subtle.rb       $homerepo/subtle.rb
+
+# [Moc]
+cp $HOME/.moc/config                    $homerepo/mocconfig
+cp $HOME/.moc/config/themes/theme       $homerepo/moctheme
+
+# [conky]
+cp $HOME/.conkyrc                       $homerepo/conkyrc
+
+cp $HOME/.config/bashrun2/bashrun2.rc   $homerepo/bashrun2.rc
+cp $HOME/.config/dunstrc                $homerepo/dunstrc
 
--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ homerepo="$HOME/dotfiles"
 cd $homerepo
 
 # [Vim]
-ln -sf ${homerepo}/vim/vimrc $HOME/.vimrc
+ln -sf $homerepo/vim/vimrc $HOME/.vimrc
 
 
