00dc2849fea416c579683b3894621e8562f43d15
carlosf <8carlosf@gmail.com>
2014-02-28 14:02:02 +0000

    added support for sudo import.sh

--- import.sh
+++ import.sh
@@ -6,6 +6,10 @@ if [ "$#" != 2 ];then
 	exit 0;
 fi
 
+if [ -n $SUDO_USER  ]; then
+  export HOME=$(bash <<< "echo ~$SUDO_USER")
+fi
+
 homerepo="$HOME/dotfiles"
 tname=$1
 fname=${2##*/}
@@ -15,7 +19,8 @@ mkdir -p $homerepo/$tname
 mv $2 $homerepo/$tname/$fname
 echo "mv $2 $homerepo/$tname/$fname"
 
-exp=$(echo "s/\/home\/$USER/\$HOME/g")
+user=${HOME##*/}
+exp=$(echo "s/\/home\/$user/\$HOME/g")
 lname=$(echo $2 | sed -e $exp)
 
 echo -e "\n# [$tname]\n#ln -sf \$homerepo/$tname/$fname $lname" >> $homerepo/install.sh
